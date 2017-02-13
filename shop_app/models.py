251f087dee01d8f6cee589d61dc8b2f8c3037d04
Adam Dyderski <adamdyderski94@gmail.com>
2017-02-13 23:14:30 +0100

    stworznie modelu produktu

--- shop_app/models.py
+++ shop_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=50, verbose_name="Nazwa")
+    description = models.TextField(max_length=4000, blank=True, verbose_name="Opis")
+    price = models.DecimalField(max_digits=6, decimal_places=2, verbose_name="Cena")
+    quantity = models.IntegerField(default=0, verbose_name="Ilość")
+    image = models.FileField(default="no_photo.png",verbose_name="Zdjęcie")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Produkt"
+        verbose_name_plural = "Produkty"
