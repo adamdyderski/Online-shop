4b46b93d7b243a6da085d852cea6d186db938080
Adam Dyderski <adamdyderski94@gmail.com>
2017-02-14 22:54:14 +0100

    strona produktu
    http://stackoverflow.com/questions/14029245/python-putting-an-if-elif-else-statement-on-one-line

--- shop_app/urls.py
+++ shop_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'shop_app'
 
 urlpatterns = [
-    url(r'^$', views.Home.as_view(), name='home')
+    url(r'^$', views.Home.as_view(), name='home'),
+    url(r'^details/(?P<pk>[0-9]+)/$', views.ProductDetails.as_view(), name='details')
 ]
--- shop_app/views.py
+++ shop_app/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import FormMixin
 
+from .forms import AddToCartForm
 from .models import Product
 
 
@@ -9,3 +11,20 @@ class Home(ListView):
     model = Product
     context_object_name = "products"
     template_name = "shop_app/home.html"
+
+
+class ProductDetails(DetailView, FormMixin):
+    model = Product
+    template_name = "shop_app/product_details.html"
+    form_class = AddToCartForm
+
+    def availability(self):
+        q = self.object.quantity
+        return 'brak' if q == 0 else 'na wyczerpaniu' if q < 5 else 'dostępny'  
+
+    def get_context_data(self, **kwargs):
+        context = super(ProductDetails, self).get_context_data(**kwargs)
+        context['availability'] = self.availability()
+        return context
+    
+    
