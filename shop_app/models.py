35eca3470b16f2b2094663cc17e715f49686f67b
Adam Dyderski <adamdyderski94@gmail.com>
2017-02-20 01:50:26 +0100

    zmiana rodzaju pola 'kategoria' na ManyToManyField

--- shop_app/models.py
+++ shop_app/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Category(models.Model):
-    name = models.CharField(max_length=50, verbose_name="Kategoria")
+    name = models.CharField(max_length=50, verbose_name="Nazwa kategorii")
     
     def __str__(self):
         return self.name
@@ -11,7 +11,7 @@ class Category(models.Model):
         verbose_name_plural = "Kategorie"
 
 class Product(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name="Kategoria")
+    category = models.ManyToManyField(Category, verbose_name="Kategorie")
     name = models.CharField(max_length=50, verbose_name="Nazwa")
     description = models.TextField(max_length=4000, blank=True, verbose_name="Opis")
     price = models.DecimalField(max_digits=6, decimal_places=2, verbose_name="Cena")
