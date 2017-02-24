b7e0a12d58369ac29985482bd5ae043abf988e18
Adam Dyderski <adamdyderski94@gmail.com>
2017-02-24 18:23:25 +0100

    poprawione dodawanie do koszyka, utworzenie strony koszyka

--- cart/urls.py
+++ cart/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'cart'
 
 urlpatterns = [
-    url(r'^add/(?P<product_pk>[0-9]+)/$', views.add, name='add')
+    url(r'^add/(?P<product_pk>[0-9]+)/$', views.add, name='add'),
+    url(r'^show/$', views.Show.as_view(), name='show')
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,10 +1,12 @@
 from django.contrib import messages
 from django.core.urlresolvers import reverse_lazy
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, render
 from django.views import View
+from django.views.generic import TemplateView
 
 from shop_app.models import Product
+
 
 def add(request, product_pk):
     
@@ -16,8 +18,16 @@ def add(request, product_pk):
             messages.info(request, 'Niestety, obecnie dostępnych sztuk: ' + str(product.quantity))
         else:
             cart = request.session.get('cart', {})
-            cart.update(product_pk=add_quantity)
+            cart.update({product.pk:add_quantity})
             request.session['cart'] = cart
             messages.success(request, 'Dodano do koszyka!')
 
     return HttpResponseRedirect(reverse_lazy('shop_app:details', kwargs={'pk': product_pk}))
+
+class Show(View):
+    template_name = 'cart/show_cart.html'
+
+    def get(self, request, *args, **kwargs):
+        cart = request.session.get('cart', {})
+        products = Product.objects.filter(pk__in=cart.keys())
+        return render(request, self.template_name, context={'products': products}) 
