ccfc8038a3d2220bb7a0ee78a44f60abf55c25df
Adam Dyderski <adamdyderski94@gmail.com>
2017-03-07 00:37:19 +0100

    dodanie informacji na liście sposobów wysyłki
    w komentarzu drugi sposób na wyświetlanie listy zakupionych produktów w zamówieniu

--- cart/admin.py
+++ cart/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(ShippingMethod)
 
+
+# class OrderProductInline(admin.TabularInline):
+#     readonly_fields = ['product','product','quantity']
+#     model = OrderProduct
+#     extra = 0
+#     can_delete = False
+#     verbose_name = 'Zamówiony Produkt'
+#     verbose_name_plural = 'Zamówione produkty'
 
 class OrderAdmin(admin.ModelAdmin):
     readonly_fields=('user_info','shipping_method','total','products')
@@ -14,4 +21,14 @@ class OrderAdmin(admin.ModelAdmin):
             ('Zamówienie:', {'fields': ('products',)}),
     )
 
+    # inlines = [
+    #     OrderProductInline,
+    # ]
+
+class ShippingMethodAdmin(admin.ModelAdmin):
+    list_display = ('name','price','days')
+    list_filter = ('price', 'days')
+    search_fields = ['name']
+
 admin.site.register(Order, OrderAdmin)
+admin.site.register(ShippingMethod, ShippingMethodAdmin)
