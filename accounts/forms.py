35959ab6d992d06c215d31c3dcacd2c19f5ecff5
Adam Dyderski <adamdyderski94@gmail.com>
2017-05-24 00:50:45 +0200

    usunięcie importa, blokada powtarzania maili

--- accounts/forms.py
+++ accounts/forms.py
@@ -8,24 +8,24 @@ from accounts.models import User
 class RegistrationFrom(UserCreationForm):
 
     rules = forms.BooleanField(required=True, initial=False, widget=forms.CheckboxInput(attrs={'data-toggle':'checkbox'}))
-    
+
     class Meta:
         model = User
         fields = ('username', 'password1', 'password2', 'first_name',
                   'last_name', 'street', 'postcode', 'city', 'email', 'phone')
-    
-    def save(self, commit=True): 
-        user = super(RegistrationFrom, self).save(commit=False) 
+
+    def save(self, commit=True):
+        user = super(RegistrationFrom, self).save(commit=False)
         # wymagana aktywacja konta poprzez link aktywacyjny
         user.is_active = False
-        if commit: 
+        if commit:
             user.save()
         return user
-    
-    # def clean_email(self):
-    #     email = self.cleaned_data.get('email')
 
-    #     if User.objects.filter(email=email).exists():
-    #         raise forms.ValidationError("Ten email został już użyty.")
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
 
-    #     return email
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("Ten email został już użyty.")
+
+        return email
